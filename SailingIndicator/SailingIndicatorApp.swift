8b1646e995a777dcbca1ee98d590e3ee09c1a012
cobalt70 <giwoo.kim@gmail.com>
2024-09-29 16:30:25 +0900

    Initial Commit

---
+++ SailingIndicator/SailingIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SailingIndicatorApp.swift
+//  SailingIndicator
+//
+//  Created by Gi Woo Kim on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SailingIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
